9d6539d6741ef3552690c6afbc6036747fe960d2
Pavan <pavannayakiict@gmail.com>
2018-09-06 12:48:01 +0530

    imputing missinkg value

--- hello.py
+++ hello.py
@@ -34,3 +34,26 @@ concat=pd.concat([mid1,mid2])
 print(concat)
 
 
+'''
+"""
+Spyder Editor
+
+This is a temporary script for basic imputation of missing data in a frame  file.
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#reading data from csv file
+dataset=pd.read_csv('Data.csv')
+X=dataset.iloc[:,:-1].values
+
+
+#taking the care of the miossing data
+from sklearn.preprocessing import Imputer
+imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputer=imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+
+'''
