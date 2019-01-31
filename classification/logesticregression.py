53a91b9113d67c9296f7aad2f2ffe4adf65e2be7
pavan <pavannayakiict@gmail.com>
2019-01-31 18:38:22 +0530

    Changes to be committed:

---
+++ classification/logesticregression.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 23:01:48 2019
+
+@author: pavan
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,1:4].values
+y = dataset.iloc[:,4].values
+
+#label encoder  for gender variables
+from sklearn.preprocessing  import LabelEncoder
+encoder = LabelEncoder()
+encoder.fit(X[:,0])
+X[:,0] = encoder.transform(X[:,0])
+
+#splitting the dataset into training and testing dataset
+from sklearn.model_selection import train_test_split
+X_train ,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+'''
+standardizing the values
+'''
+from sklearn.preprocessing import StandardScaler
+sc_Xtrain = StandardScaler()
+sc_Xtest = StandardScaler()
+
+X_train = sc_Xtrain.fit_transform(X_train)
+X_test =  sc_Xtest.fit_transform(X_test)
+
+#fitting the llogestic regression model 
+from sklearn.linear_model import LogisticRegression
+classifier  = LogisticRegression(random_state = 0)
+classifier.fit(X_train,y_train)
+
+
+#predicting the result's
+y_pred = classifier.predict(X_test)
+
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
