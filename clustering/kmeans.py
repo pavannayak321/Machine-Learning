73f45f13b815620dc6aa28a168f3881474a2f39e
pavan <pavannayakiict@gmail.com>
2019-02-03 14:48:01 +0530

    KMeans

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -32,16 +32,22 @@ plt.show()
 #As we can conclude from the graph that we can best use the no of clusters as 3
 #fit the model with the 3 no of clusters 
 
-kmeans = KMeans(n_clusters=3,init='k-means++',n_init=10,random_state=0)
+kmeans = KMeans(n_clusters=5,init='k-means++',n_init=10,random_state=0)
 y_means = kmeans.fit_predict(X)
 
 
 #visualizing the clusters of different type's
 plt.scatter(X[y_means==0,0],X[y_means==0,1],c='red',s=100)
-plt.scatter(X[y_means==1,0],X[y_means==1,1],c='green',s=100)
-plt.scatter(X[y_means==2,0],X[y_means==2,1],c='blue',s=100)
+plt.scatter(X[y_means==1,0],X[y_means==1,1],c='blue',s=100)
+plt.scatter(X[y_means==2,0],X[y_means==2,1],c='green',s=100)
 plt.scatter(X[y_means==3,0],X[y_means==3,1],c='cyan',s=100)
+plt.scatter(X[y_means==4,0],X[y_means==4,1],c='magenta',s=100)
 
 plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow' ,label='centroids')
+plt.title('Cluster of Clients')
+plt.xlabel('Annual Income')
+plt.ylabel('Spending Score')
+plt.legend()
+plt.show()
 
 
