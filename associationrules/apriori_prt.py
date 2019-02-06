9e27bb372ceefa0ab600a5edc79c08c5accdc0b9
pavan <pavannayakiict@gmail.com>
2019-02-06 21:41:06 +0530

    Changes to be committed:

---
+++ associationrules/apriori_prt.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  3 21:09:45 2019
+
+@author: pavan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#reading the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv',header=None).values
+
+
